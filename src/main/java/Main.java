c690ae8937939506e1aff3c0ea0665f35b196210
Razhabka <orlov.22801@gmail.com>
2024-11-29 15:50:49 +0300

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,14 +8,23 @@ public class Main {
         checkSumSing();
         printColor();
         compareNumbers();
-        System.out.println(withinNumbers(10, 7)+ "\n");
+        System.out.println(withinNumbers(10, 7) + "\n");
         positiveOrNegativeNum(-10);
         System.out.println(positiveOrNegativeNumBool(10));
+        printStrings("Hello", 5);
+        System.out.println(checkYear(1600) + "\n");
+        lst01();
+        lst02();
+        lst03();
+        lst04();
+        System.out.println(Arrays.toString(createArray(10, 5)));
+
     }
 
 
     public static void printTree(){
 
+        System.out.println("Задание №1");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple\n");
@@ -22,32 +33,35 @@ public class Main {
 
     public static void checkSumSing(){
 
+        System.out.println("Задание №2");
         int a = 23;
         int b = 78;
 
         if(a + b >= 0){
-            System.out.println("Сумма положительная");
+            System.out.println("Сумма положительная\n");
         }else {
-            System.out.println("Сумма отрицательная");
+            System.out.println("Сумма отрицательная\n");
         }
 
     }
 
     public static void printColor(){
 
+        System.out.println("Задание №3");
         int value = 143;
 
         if (value == 0){
-            System.out.println("\nКрасный\n");
-        }else if ( value > 0 && value <= 100  ) {
-            System.out.println("\nЖелтый\n");
+            System.out.println("Красный\n");
+        }else if ( (value > 0) && (value <= 100)) {
+            System.out.println("Желтый\n");
         }else if (value > 100) {
-            System.out.println("\nЗеленый\n");
+            System.out.println("Зеленый\n");
         }
     }
 
     public static void compareNumbers(){
 
+        System.out.println("Задание №4");
         int a = -10;
         int b = 34;
 
@@ -61,12 +75,14 @@ public class Main {
 
     public static boolean withinNumbers (int num1, int num2){
 
-        return num1 + num2 >=10 && num1+num2 <= 20;
+        System.out.println("Задание №5");
+        return (num1 + num2 >=10) && (num1+num2 <= 20);
 
     }
 
     public static void positiveOrNegativeNum(int num){
 
+        System.out.println("Задание №6");
         if(num >= 0){
             System.out.println("Число положительное\n");
         }else {
@@ -78,6 +94,7 @@ public class Main {
 
     public static boolean positiveOrNegativeNumBool(int num){
 
+        System.out.println("Задание №7");
         if(num >= 0){
             return false;
         }else {
@@ -86,6 +103,108 @@ public class Main {
 
     }
 
+    public static void printStrings(String str, int num){
+
+        System.out.println();
+        System.out.println("Задание №8");
+        for(int i = 0; i < num; i++){
+            System.out.print(str + " ");
+        }
+        System.out.println("\n");
+
+    }
+
+    public static boolean checkYear (int year){
+
+        System.out.println("Задание №9");
+        if((year % 4 == 0) && (year % 100 != 0)){
+            return true;
+        }
+        else if (year % 400 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public static void lst01(){
+
+        System.out.println("Задание №10");
+        int[] binaryCode = new int[]{0,1,0,0,1,0,1,1};
+        System.out.println(Arrays.toString(binaryCode));
+
+        for (int i = 0; i < binaryCode.length; i++){
+            if(binaryCode[i] == 0){
+                binaryCode[i] = 1;
+            }
+            else {
+                binaryCode[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(binaryCode) + "\n");
+
+    }
+
+    public static void lst02(){
+
+        System.out.println("Задание №11");
+        int[] lst = new int[100];
+        for(int i = 0; i < lst.length; i++){
+            lst[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(lst) + "\n");
+
+    }
+
+    public static void lst03(){
+
+        System.out.println("Задание №12");
+        int[] lst = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(lst));
+        for(int i = 0; i < lst.length; i++){
+            if(lst[i] < 6){
+                lst[i] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(lst) + "\n");
+
+    }
+
+    public static void lst04(){
+
+        System.out.println("Задание №13");
+        int[][] arr = new int[3][3];
+        /*
+        {{0,0,0,}
+         {0,0,0,}
+         {0,0,0,}}
+         */
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if((i == 0 && j == 0) || (i == 1 && j == 1) || (i == 2 && j == 2) || (i == 0 && j == 2) || (i == 2 && j == 0)){
+                    arr[i][j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(Arrays.toString(arr[i]));
+        }
 
 
+    }
+
+    public static int[] createArray(int len, int initialValue){
+
+        System.out.println();
+        System.out.println("Задание №14");
+        int[] lst = new int[len];
+        for(int i = 0; i < lst.length; i++){
+            lst[i] = initialValue;
+        }
+        return lst;
+
+    }
 }
